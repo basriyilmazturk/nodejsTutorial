cfe68c2adce4bec1c108ad4470c87a1eae6ca5e6
basriyilmazturk <basriyilmazturk@gmail.com>
2020-12-23 02:39:01 +0300

    nodejs request and routing

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-var http=require('http')
-var server=http.createServer((function(request,response)
+// var http=require('http')
+// var server=http.createServer((function(request,response)
+// {
+//     response.writeHead(200,
+//         {"Content-Type" : "text/plain"});
+//     response.end("Hello World\n");
+// }));
+// server.listen(3000);
+
+var express=require('express');
+var app=express();
+app.get('/',function(req,res)
 {
-    response.writeHead(200,
-        {"Content-Type" : "text/plain"});
-    response.end("Hello World\n");
-}));
-server.listen(7000);
+    res.send('Hello World!');
+});
+
+app.route('/Node').get(function(req,res)
+{
+    res.send("Tutorial on Node");
+});
+app.route('/Angular').get(function(req,res)
+{
+    res.send("Tutorial on Angular");
+});
+app.get('/',function(req,res){
+    res.send('Welcome to Guru99 Tutorials');
+});
+
+var server=app.listen(3000,function() {});
