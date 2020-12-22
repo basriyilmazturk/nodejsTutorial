09fa2144f57e3ea9f29937d9577f1cd5a6172a2b
basriyilmazturk <basriyilmazturk@gmail.com>
2020-12-23 02:26:32 +0300

    hello world on http

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-var Addition=require('./Addition.js');
-console.log(Addition.AddNumber(1,2));
+var http=require('http')
+var server=http.createServer((function(request,response)
+{
+    response.writeHead(200,
+        {"Content-Type" : "text/plain"});
+    response.end("Hello World\n");
+}));
+server.listen(7000);
